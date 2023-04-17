a9828a93f09e2e8d0e1ff29e0fe7db76b7ef591b
Lucas <lucas.silva@agencian1.com.br>
2023-04-16 23:30:17 -0300

    Start do projeto

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,15 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const router = express.Router();
+
+class Test {
+    show(req: Request, res: Response){
+        return res.json({oi: "Olá"});
+    }
+}
+
+const testController = new Test();
+
+router.get("/", testController.show)
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 import router from "./routes";
 
 const api = express();
 
+api.use(cors({
+    origin: ["http://localhost:5055/"]
+}))
 api.use(router);
 
 api.listen("5055");
